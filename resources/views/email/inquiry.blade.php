bc8c31e9164ff6b610c883a55ddb96846f2b351a
ArdiDwiWibowo07 <ardw.wibowo@gmail.com>
2023-12-13 16:01:23 +0700

    Updated

--- resources/views/email/inquiry.blade.php
+++ resources/views/email/inquiry.blade.php
@@ -35,38 +35,40 @@
             <td style="width: 20%">Description</td>
             <td>: {{ $data['description'] }}</td>
         </tr>
-        <tr style="vertical-align: top">
+        <tr>
             <td style="width: 20%">Product List</td>
-            <td>: <table style="width: 100%; border: 1px solid;">
-                    <thead>
-                        <tr>
-                            <th style="width: 20%; border: 1px solid;">ITEM NAME</th>
-                            <th style="width: 20%; border: 1px solid;">MATERIAL DESCRIPTION</th>
-                            <th style="width: 20%; border: 1px solid;">SIZE</th>
-                            <th style="width: 20%; border: 1px solid;">QTY</th>
-                            <th style="width: 20%; border: 1px solid;">REMARK</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($data['products'] as $p)
-                        <tr>
-                            <td style="border: 1px solid;">{{ $p->item_name }}</td>
-                            <td style="border: 1px solid;">{{ $p->description }}</td>
-                            <td style="border: 1px solid;">{{ $p->size }}</td>
-                            <td style="border: 1px solid;">{{ $p->qty }}</td>
-                            <td style="border: 1px solid;">{{ $p->remark }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </td>
+            <td>:</td>
         </tr>
 
 
     </table>
 
     <br>
+    <table style="width: 100%; border: 1px solid;">
+        <thead>
+            <tr>
+                <th style="width: 20%; border: 1px solid;">ITEM NAME</th>
+                <th style="width: 20%; border: 1px solid;">MATERIAL DESCRIPTION</th>
+                <th style="width: 20%; border: 1px solid;">SIZE</th>
+                <th style="width: 20%; border: 1px solid;">QTY</th>
+                <th style="width: 20%; border: 1px solid;">REMARK</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data['products'] as $p)
+            <tr>
+                <td style="border: 1px solid;">{{ $p->item_name }}</td>
+                <td style="border: 1px solid;">{{ $p->description }}</td>
+                <td style="border: 1px solid;">{{ $p->size }}</td>
+                <td style="border: 1px solid;">{{ $p->qty }}</td>
+                <td style="border: 1px solid;">{{ $p->remark }}</td>
+            </tr>
+            @endforeach
+        </tbody>
 
+
+
+    </table>
 </body>
 
 </html>
