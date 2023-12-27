7e27149428c4f533dcdc1828bf496bb7dfef0d9e
ArdiDwiWibowo07 <ardw.wibowo@gmail.com>
2023-12-27 14:15:46 +0700

    [Updated]

--- resources/views/crm/inquiry/index.blade.php
+++ resources/views/crm/inquiry/index.blade.php
@@ -808,7 +808,11 @@
                             <th class="text-center">No.</th>
                             <th class="text-center">ID Visit</th>
                             <th class="text-center">ID Inquiry</th>
-                            <th class="text-center">Customer - Company Name</th>
+                            <th class="text-center">Customer - Company Name</th>`+
+                            @if (auth()->user()->hasRole('admin_sales') || auth()->user()->hasRole('superadmin') || auth()->user()->hasRole('manager') || auth()->user()->hasRole('hod'))
+                            `<th class="text-center">Sales</th>`
+                            @endif
+                            +`
                             <th class="text-center">Date</th>
                             <th class="text-center">Grade</th>
                             <th class="text-center">Status</th>
@@ -849,6 +853,11 @@
                     {
                         data: "customer"
                     },
+                    @if (auth()->user()->hasRole('admin_sales') || auth()->user()->hasRole('superadmin') || auth()->user()->hasRole('manager') || auth()->user()->hasRole('hod'))
+                    {
+                        data: "sales"
+                    },
+                    @endif
                     {
                         data: "date"
                     },
