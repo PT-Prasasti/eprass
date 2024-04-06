4c708db205e696119060124f65b397f6097c4cab
XCOD33 <denistraju155@gmail.com>
2024-04-06 11:02:15 +0700

    feat(controller): add finance role to payment request
    Add finance role to payment request controller to allow finance to approve payment requests.
    
    BREAKING CHANGE: The payment request approval process has been updated to include finance.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -157,12 +157,10 @@
                         <a href="{{ route('payment-request.exim') }}"><i class="fa fa-calendar-check-o"></i><span
                                 class="sidebar-mini-hide">App Payment Request</span></a>
                     </li>
-
-                    
                 @endif
 
                 @if (auth()->user()->hasRole('hod'))
-                <li class="nav-main-heading">
+                    <li class="nav-main-heading">
                         <span class="sidebar-mini-hidden">SALES</span>
                     </li>
                     <li>
@@ -175,8 +173,8 @@
                     </li>
                     <li>
                     <li>
-                        <a href="{{ route('crm.inquiry') }}"><i class="fa fa-edit"></i><span class="sidebar-mini-hide"
-                                id="inquiry-nav">List Inquiry</span></a>
+                        <a href="{{ route('crm.inquiry') }}"><i class="fa fa-edit"></i><span
+                                class="sidebar-mini-hide" id="inquiry-nav">List Inquiry</span></a>
                     </li>
 
                     <li class="nav-main-heading">
@@ -236,7 +234,7 @@
                                     <a href="{{ route('transaction.sourcing-item') }}">List Sourcing Item</a>
                                 </li>
                             </ul>
-                    
+
                     <li>
                         <a class="nav-submenu" data-toggle="nav-submenu" href="#">
                             <i class="fa fa-book"></i>
@@ -274,10 +272,10 @@
                         <a href="{{ route('payment-request.exim') }}"><i class="fa fa-calendar-check-o"></i><span
                                 class="sidebar-mini-hide">App Payment Request</span></a>
                     </li>
-                    
 
 
-                    
+
+
 
 
                     <li class="nav-main-heading">
@@ -676,7 +674,7 @@
                         <span class="sidebar-mini-hidden">ORDER SUPPLIER</span>
                     </li>
                     <li>
-                        <a class="active" href="{{ route('list-payment') }}"><i class="fa fa-file"></i><span
+                        <a class="active" href="{{ route('payment-request.exim') }}"><i class="fa fa-file"></i><span
                                 class="sidebar-mini-hide">List Payment</span></a>
                     </li>
                 @endif
